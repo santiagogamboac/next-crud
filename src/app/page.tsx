db78de0964e39c5fe0a8301fed88e37f1e3a8a1d
santiagogamboac <santiagogamboacely@gmail.com>
2025-04-06 13:09:21 -0500

    Contact Info y Testimnonials Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Hero from "@/app/components/hero"
 import ServicesSection from "@/app/components/services-section"
-import TestimonialsSection from "@/app/components/testimonials-section"
+import TestimonialsSection from "@/app/components/TestimonialsSection"
 // import CtaSection from "@/components/cta-section"
-import ContactInfo from "@/app/components/contact-info"
+import ContactInfo from "@/app/components/ContactInfo"
 
 export default function Home() {
   return (
